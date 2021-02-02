1290ac59ed6399d3c72921edf208120c7b0c2368
Igor Semenov <semenow.igorek@yandex.ru>
2021-02-02 16:50:14 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <shop shop-title="Смартфоны" />
+    <shop :shop-title="{title: 'Ноутбуки'}" />
+  </div>
+</template>
+
+<script>
+  import Shop from "./components/Shop";
+
+  export default {
+    components: {
+      Shop
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+#app {
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Todo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <input type="text" v-model="todoText" placeholder="Ваша задача">
+        <button @click="add" :disabled="!todoText">Добавить</button>
+        <button @click="deleteAll">Удалить все</button>
+
+        <div v-for="(item, key) in items" :key="key">
+            <span>{{ key }} - {{ item.text }}</span>
+            <button @click="deleteEl(key)">Удалить</button>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Todo",
+    data() {
+      return {
+        todoText: '',
+        items: []
+      }
+    },
+    methods: {
+      add() {
+        this.items.push({text: this.todoText})
+      },
+
+      deleteEl(key) {
+        this.items.splice(key, 1)
+      },
+
+      deleteAll() {
+        this.items = [];
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
